de7a8dde6352e83979d06b7bb63a8b2af1602c07
chan-nni <rkdcksal02@gmail.com>
2024-04-16 17:33:36 +0900

    vue board test

--- src/main.js
+++ src/main.js
@@ -1,4 +1,5 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import router from './router' // Vue Router import
 
-createApp(App).mount('#app')
+createApp(App).use(router).mount('#app')
---
+++ src/views/BoardView.vue
@@ -0,0 +1,34 @@
+<template>
+  <div>
+    <h1>게시판</h1>
+    <ul>
+      <li v-for="board in boards" :key="board.id">
+        {{ board.title }}
+        {{ board.nickname}}</li>
+    </ul>
+
+  </div>
+</template>
+
+<script>
+import axios from 'axios';
+
+export default {
+  data() {
+    return {
+      boards: []
+    };
+  },
+  mounted() {
+    // 게시글 목록을 가져오는 API 호출
+    axios.get('http://localhost:7777/boards')
+        .then(response => {
+          // API 호출에 성공하면 데이터를 저장
+          this.boards = response.data.content;
+        })
+        .catch(error => {
+          console.error('API 호출에 실패했습니다:', error);
+        });
+  }
+};
+</script>
