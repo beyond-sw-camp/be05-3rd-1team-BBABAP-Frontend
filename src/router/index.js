727f42edfb2b26a7d74df3227c8ef6bb7847da86
dongwook <hsw50108@gmail.com>
2024-04-16 21:16:36 +0900

    add : LoginView, RegisterView

--- src/router/index.js
+++ src/router/index.js
@@ -1,7 +1,9 @@
 // router/index.js
 import { createRouter, createWebHistory } from 'vue-router';
 import HomeView from '../views/HomeView.vue';
-import BoardView from '../views/BoardView.vue'; // BoardView 컴포넌트 import
+import BoardView from '../views/BoardView.vue';
+import RegisterView from "@/views/RegisterView";
+import LoginView from "@/views/LoginView"; // BoardView 컴포넌트 import
 
 const routes = [
   {
@@ -18,6 +20,16 @@ const routes = [
     path: '/boards', // '/boards' 경로 추가
     name: 'boards',
     component: BoardView // BoardView 컴포넌트 추가
+  },
+  {
+    path: '/register', // '/boards' 경로 추가
+    name: 'register',
+    component: RegisterView // BoardView 컴포넌트 추가
+  },
+  {
+    path: '/login', // '/boards' 경로 추가
+    name: 'login',
+    component: LoginView // BoardView 컴포넌트 추가
   }
 ];
 
---
+++ src/views/LoginView.vue
@@ -0,0 +1,49 @@
+<template>
+  <div>
+    <h1>로그인</h1>
+    <form @submit.prevent="loginUser">
+      <label htmlFor="email">이메일:</label>
+      <input type="email" id="email" v-model="email" required>
+
+      <label htmlFor="password">비밀번호:</label>
+      <input type="password" id="password" v-model="password" required>
+
+      <button type="submit">로그인</button>
+    </form>
+  </div>
+</template>
+
+<script>
+import axios from 'axios';
+
+export default {
+  data() {
+    return {
+      email: '',
+      password: ''
+    };
+  },
+  methods: {
+    loginUser() {
+      // 폼 데이터 생성
+      let formData = new FormData();
+      formData.append('email', this.email);
+      formData.append('password', this.password);
+
+      axios.post('http://localhost:7777/login', formData)
+      .then(response => {
+        console.log('로그인 성공:', response.data);
+        // 로그인 성공 후 다음 작업 수행
+      })
+      .catch(error => {
+        console.error('로그인 실패:', error);
+        // 로그인 실패 시 처리
+      });
+    }
+  }
+};
+</script>
+
+<style scoped>
+/* 필요한 스타일 작성 */
+</style>
