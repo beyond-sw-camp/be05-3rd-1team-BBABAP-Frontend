d43d6b3cbb7a753e52c1686f6b7ad75c48847bd7
zzimni <hjm3327@naver.com>
2024-04-17 16:08:53 +0900

    git test

--- src/main.js
+++ src/main.js
@@ -1,14 +1,13 @@
-import './assets/common.css'
+import { createApp } from 'vue';
+import App from './App.vue';
+import router from './router';
+import axios from 'axios';
+// import store from './store/store'; // Vuex store import
 
-import { createApp } from 'vue'
-import App from './App.vue'
-import router from './router'
-import axios from 'axios'
-import SummernoteEditor from 'vue3-summernote-editor';
+const app = createApp(App);
 
+app.config.globalProperties.$axios = axios;  
+app.config.globalProperties.$serverUrl = '//localhost:7777'; 
 
-const app = createApp(App)
-app.config.globalProperties.$axios = axios;  //전역변수로 설정 컴포넌트에서 this.$axios 호출할 수 있음
-app.config.globalProperties.$serverUrl = '//localhost:7777' // api server
-app.component('SummernoteEditor', SummernoteEditor);
-app.use(router).mount('#app')
+// app.use(store);
+app.use(router).mount('#app');
