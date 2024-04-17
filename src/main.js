e38c97fd6cfbef58ca10360d23534125f302ebf0
chan-nni <rkdcksal02@gmail.com>
2024-04-17 20:44:15 +0900

    웹에디터 미완

--- src/main.js
+++ src/main.js
@@ -3,13 +3,21 @@ import App from './App.vue';
 import router from './router';
 import axios from 'axios';
 import store from './store/store'; // Vuex store import
+import { createQuillEditor } from 'vue-quill-editor'; // 변경된 import 문
+import 'quill/dist/quill.core.css'; // Quill 에디터의 스타일을 불러옵니다.
+import 'quill/dist/quill.snow.css'; // 선택적으로 테마 스타일을 불러올 수 있습니다.
 
 const app = createApp(App);
+
+// QuillEditor를 컴포넌트로 등록
+const { QuillEditor } = createQuillEditor();
+
+app.component('QuillEditor', QuillEditor);
 
 // Vuex를 Vue에 등록
 app.use(store);
 
-app.config.globalProperties.$axios = axios;  
-app.config.globalProperties.$serverUrl = '//localhost:7777'; 
+app.config.globalProperties.$axios = axios;
+app.config.globalProperties.$serverUrl = '//localhost:7777';
 
 app.use(router).mount('#app');
