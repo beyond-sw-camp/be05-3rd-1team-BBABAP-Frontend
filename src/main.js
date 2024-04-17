354ee6d056dbf3b9e126332c54e644e5cfc09ce5
eujin99 <pinklusha999@gmail.com>
2024-04-17 17:31:18 +0900

    로그인 유지

--- src/main.js
+++ src/main.js
@@ -13,3 +13,5 @@ app.config.globalProperties.$axios = axios;
 app.config.globalProperties.$serverUrl = '//localhost:7777'; 
 
 app.use(router).mount('#app');
+// 앱이 로드될 때 로컬 스토리지에서 사용자 정보를 가져와 Vuex에 복원
+store.commit('restoreUser');
