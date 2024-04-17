ec189ab9c8661c966a6183c34c471e9170152ee6
YoungWon <nbbb9@naver.com>
2024-04-17 20:52:30 +0900

    필요없는거 지움

--- src/router/index.js
+++ src/router/index.js
@@ -1,9 +1,6 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import BoardList from '@/views/board/BoardList.vue'
-import BoardDetail from '@/views/board/BoardDetail.vue'
 import SignupForm from '@/components/SignupForm.vue';
 import LoginForm from '@/components/LoginForm.vue';
-import BoardListJsonServer from '../views/BoardListJsonServer';
 import PostForm from '../views/PostForm.vue';
 import BoardView from '../views/BoardView.vue';
 
@@ -22,21 +19,6 @@ const routes = [
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
     component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
-  },
-  {
-    path: '/boardlist',
-    name: 'BoardList',
-    component: BoardListJsonServer // 나중에 BoardList.vue로 바꿔주기
-  },
-  {
-    path: '/board/list',
-    name: 'BoardList2',
-    component: BoardList
-  },
-  {
-    path: '/board/detail',
-    name: 'BoardDetail',
-    component: BoardDetail
   },
   {
     path: '/signup',
